d70a8d70cfd7693515a04d85f9cd74ccf5ad72f0
Dmitriy Vilkov <dav.daemon@gmail.com>
2016-09-06 00:11:41 +0300

    Add 'FindBrolly.cmake' script

---
+++ cmake/FindBrolly.cmake
@@ -0,0 +1,23 @@
+# - Find brolly
+# Find the brolly library
+#
+#  This module defines the following variables:
+#     BROLLY_FOUND   - True if BROLLY_INCLUDE is found
+#     BROLLY_INCLUDE - where to find header files
+#     BROLLY_LIB     - the library files
+
+
+find_path (BROLLY_INCLUDE
+           NAMES "brolly/assert.h"
+           PATH_SUFFIXES include
+           DOC "Brolly include directory")
+
+find_library (BROLLY_LIB
+              NAMES brolly
+              DOC "Brolly library file")
+
+# handle the QUIETLY and REQUIRED arguments and set BROLLY_FOUND to TRUE if all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (Brolly DEFAULT_MSG BROLLY_INCLUDE BROLLY_LIB)
+
+mark_as_advanced (BROLLY_INCLUDE BROLLY_LIB)
