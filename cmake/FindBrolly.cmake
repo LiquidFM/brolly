3a70e8698b939711d084b81a0ea8274983d20a3b
Dmitriy Vilkov <dav.daemon@gmail.com>
2016-09-06 01:14:55 +0300

    Update 'Find' CMake script

--- cmake/FindBrolly.cmake
+++ cmake/FindBrolly.cmake
@@ -7,17 +7,24 @@
 #     BROLLY_LIB     - the library files
 
 
-find_path (BROLLY_INCLUDE
-           NAMES "brolly/assert.h"
-           PATH_SUFFIXES include
-           DOC "Brolly include directory")
+if (NOT BROLLY_FOUND)
+    find_path (BROLLY_INCLUDE
+	       NAMES "brolly/assert.h"
+	       PATH_SUFFIXES include
+	       DOC "Brolly include directory")
 
-find_library (BROLLY_LIB
-              NAMES brolly
-              DOC "Brolly library file")
+    find_library (BROLLY_LIB
+		  NAMES brolly
+		  DOC "Brolly library file")
 
-# handle the QUIETLY and REQUIRED arguments and set BROLLY_FOUND to TRUE if all listed variables are TRUE
-include (FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS (Brolly DEFAULT_MSG BROLLY_INCLUDE BROLLY_LIB)
+    # handle the QUIETLY and REQUIRED arguments and set BROLLY_FOUND to TRUE if all listed variables are TRUE
+    include (FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS (Brolly DEFAULT_MSG BROLLY_INCLUDE BROLLY_LIB)
 
-mark_as_advanced (BROLLY_INCLUDE BROLLY_LIB)
+    find_package (Platform REQUIRED)
+    list (APPEND BROLLY_INCLUDE ${PLATFORM_INCLUDE})
+
+    list (REMOVE_DUPLICATES BROLLY_INCLUDE)
+
+    mark_as_advanced (BROLLY_INCLUDE BROLLY_LIB)
+endif ()
